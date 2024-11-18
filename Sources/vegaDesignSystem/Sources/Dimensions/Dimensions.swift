fce69d5d622f78a066a045717d0a158321cb928f
davilinho <davilinho@gmail.com>
2024-11-18 19:19:39 +0100

    New dimensions

---
+++ Sources/vegaDesignSystem/Sources/Dimensions/Dimensions.swift
@@ -0,0 +1,19 @@
+//
+//  Dimensions.swift
+//  vegaDesignSystem
+//
+//  Created by David Martin Nevado on 18/11/24.
+//
+
+import SwiftUI
+
+public struct Dimensions {
+    public static let ZERO: Double = 0.0
+    public static let XS: Double = 4.0
+    public static let S: Double = 8.0
+    public static let M: Double = 16.0
+    public static let L: Double = 24.0
+    public static let XL: Double = 32.0
+    public static let XXL: Double = 48.0
+    public static let XXXL: Double = 64.0
+}
