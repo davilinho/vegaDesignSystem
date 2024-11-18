ec76f50ed9c57108e917950c49d230c018dc762f
davilinho <davilinho@gmail.com>
2024-11-18 18:28:11 +0100

    Make public colors

--- Sources/vegaDesignSystem/Sources/Color/AppColor.swift
+++ Sources/vegaDesignSystem/Sources/Color/AppColor.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct AppColor {
+public struct AppColor {
     let background: Color = Color("background")
     let primary: Color = Color("primary")
     let secondary: Color = Color("secondary")
